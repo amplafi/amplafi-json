b182ac5a9b1c5ab290b8152acb7f80df79cdff4a
patmoore <patmoore@amplafi.com>
2009-03-09 23:52:15 +0000

    fix exception that shows up when configuring rss feeds
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-json/trunk@206 6eaf30a2-012b-4626-ba8e-69dab18af4f2

--- src/main/java/org/amplafi/json/JSONObject.java
+++ src/main/java/org/amplafi/json/JSONObject.java
@@ -91,47 +91,6 @@ import org.apache.commons.lang.StringEscapeUtils;
 public class JSONObject implements JsonConstruct {
 
     /**
-     * JSONObject.NULL is equivalent to the value that JavaScript calls null,
-     * whilst Java's null is equivalent to the value that JavaScript calls
-     * undefined.
-     */
-    private static final class Null {
-
-        /**
-         * There is only intended to be a single instance of the NULL object,
-         * so the clone method returns itself.
-         * @return     NULL.
-         */
-        @Override
-        protected final Object clone() {
-            return this;
-        }
-
-
-        /**
-         * A Null object is equal to the null value and to itself.
-         * @param object    An object to test for nullness.
-         * @return true if the object parameter is the JSONObject.NULL object
-         *  or null.
-         */
-        @Override
-        public boolean equals(Object object) {
-            return object == null || object == this;
-        }
-
-
-        /**
-         * Get the "null" string value.
-         * @return The string "null".
-         */
-        @Override
-        public String toString() {
-            return "null";
-        }
-    }
-
-
-    /**
      * The hash map where the JSONObject's properties are kept.
      * LinkedHashMap makes testing easier (particularly when converting to strings)
      */
