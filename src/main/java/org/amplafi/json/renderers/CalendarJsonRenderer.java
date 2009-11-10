48b45b26ba7da00f03da6f803adc990e50b895f1
Patrick Moore <transparentpolitics@gmail.com>
2009-11-10 04:01:08 +0000

    comment
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-json/trunk@490 6eaf30a2-012b-4626-ba8e-69dab18af4f2

--- src/main/java/org/amplafi/json/renderers/CalendarJsonRenderer.java
+++ src/main/java/org/amplafi/json/renderers/CalendarJsonRenderer.java
@@ -5,7 +5,6 @@ import java.util.TimeZone;
 
 import org.amplafi.json.IJsonWriter;
 import org.amplafi.json.JSONObject;
-import org.amplafi.json.JSONWriter;
 import org.amplafi.json.JsonRenderer;
 
 public class CalendarJsonRenderer implements JsonRenderer<Calendar> {
@@ -28,6 +27,7 @@ public class CalendarJsonRenderer implements JsonRenderer<Calendar> {
         }
         JSONObject jsonObject = JSONObject.toJsonObject(value);
         long millis = jsonObject.getLong(TIME_IN_MILLIS);
+        // TODO: shouldn't TimezoneFlowTranslator mean this is not needed?
         String timeZoneId = jsonObject.getString(TIMEZONE_ID);
         Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(millis);
