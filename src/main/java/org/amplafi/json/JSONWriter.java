b85c3ca8dd000299d6b17dcc9e05660057d242c1
patmoore <patmoore@amplafi.com>
2010-05-19 06:32:53 +0000

    java doc
    git-svn-id: https://svn1.hosted-projects.com/transparentpolitics/amplafi-flow/amplafi-json/trunk@740 6eaf30a2-012b-4626-ba8e-69dab18af4f2

--- src/main/java/org/amplafi/json/JSONWriter.java
+++ src/main/java/org/amplafi/json/JSONWriter.java
@@ -267,8 +267,10 @@ public class JSONWriter implements IJsonWriter {
             }
         }
     }
+
     /**
-     * @see org.amplafi.json.IJsonWriter#keyValueIfNotBlankValue(java.lang.String, java.lang.String)
+     *
+     * @see org.amplafi.json.IJsonWriter#keyValueIfNotBlankValue(java.lang.Object, java.lang.String)
      */
     public <K> IJsonWriter keyValueIfNotBlankValue(K key, String value) {
         if ( !StringUtils.isBlank(value)) {
@@ -278,7 +280,8 @@ public class JSONWriter implements IJsonWriter {
     }
 
     /**
-     * @see org.amplafi.json.IJsonWriter#keyValueIfNotNullValue(java.lang.String, java.lang.Object)
+     *
+     * @see org.amplafi.json.IJsonWriter#keyValueIfNotNullValue(java.lang.Object, java.lang.Object)
      */
     public <K,V> IJsonWriter keyValueIfNotNullValue(K key, V value) {
         if ( value != null) {
