838c59d2b789d9df9ad7f07ca556b3b4e11bf63e
Patrick Moore <patmoore@farreach.es>
2013-08-09 17:41:10 -0700

    Revert "added simple date format - a bit hackish"
    This reverts commit 1f75e7ebe30a5c84d0f734d8a074c86dec13b9bb.
    
    ISO8601 format must be used.

--- src/main/java/org/amplafi/json/renderers/CalendarJsonRenderer.java
+++ src/main/java/org/amplafi/json/renderers/CalendarJsonRenderer.java
@@ -1,9 +1,6 @@
 package org.amplafi.json.renderers;
 
-import java.text.DateFormat;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
 import java.util.TimeZone;
 
 import org.amplafi.json.IJsonWriter;
@@ -24,27 +21,16 @@ public class CalendarJsonRenderer implements JsonRenderer<Calendar> {
 
     @SuppressWarnings("unchecked")
     public <K> K fromJson(Class<K> clazz, Object value, Object... parameters) {
-    	Calendar cal = Calendar.getInstance();
-    	if(value == null) {
+        if(value == null) {
             return null;
         }
-        if(((String)value).startsWith("{")){
         JSONObject jsonObject = JSONObject.toJsonObject(value);
         long millis = jsonObject.getLong(TIME_IN_MILLIS);
         // TODO: shouldn't TimezoneFlowTranslator mean this is not needed?
         String timeZoneId = jsonObject.getString(TIMEZONE_ID);
+        Calendar cal = Calendar.getInstance();
         cal.setTimeInMillis(millis);
         cal.setTimeZone(TimeZone.getTimeZone(timeZoneId));
-        }else{
-        	DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        	// HACK (Bruno): this is a hack, probably the worst way to deserialize dates.
-        	try{
-        		Date d = df.parse((String)value);
-        		cal.setTime(d);
-        	}catch(Exception e){
-        		return null;
-        	}
-        }
         return (K)cal;
     }
     public Class<? extends Calendar> getClassToRender() {
