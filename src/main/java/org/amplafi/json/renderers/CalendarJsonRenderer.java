f8db5a26a0cc99e49a3d42200396245a43a83003
Patrick Moore <patmoore@farreach.es>
2013-08-26 12:48:18 -0700

    don't create Calendars unnecessarily

--- src/main/java/org/amplafi/json/renderers/CalendarJsonRenderer.java
+++ src/main/java/org/amplafi/json/renderers/CalendarJsonRenderer.java
@@ -16,44 +16,49 @@ public class CalendarJsonRenderer implements JsonRenderer<Calendar> {
      *
      */
     public static final String TIMEZONE_ID = "timeZoneID";
+
     /**
      *
      */
     public static final String TIME_IN_MILLIS = "timeInMillis";
+
     public static final CalendarJsonRenderer INSTANCE = new CalendarJsonRenderer();
 
     @SuppressWarnings("unchecked")
     public <K> K fromJson(Class<K> clazz, Object value, Object... parameters) {
-    	Calendar cal = Calendar.getInstance();
-    	if(value == null) {
+        if (value == null) {
             return null;
         }
-        if(((String)value).startsWith("{")){
-        JSONObject jsonObject = JSONObject.toJsonObject(value);
-        long millis = jsonObject.getLong(TIME_IN_MILLIS);
-        // TODO: shouldn't TimezoneFlowTranslator mean this is not needed?
-        String timeZoneId = jsonObject.getString(TIMEZONE_ID);
-        cal.setTimeInMillis(millis);
-        cal.setTimeZone(TimeZone.getTimeZone(timeZoneId));
-        }else{
-        	DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
-        	// HACK (Bruno): this is a hack, probably the worst way to deserialize dates.
-        	try{
-        		Date d = df.parse((String)value);
-        		cal.setTime(d);
-        	}catch(Exception e){
-        		return null;
-        	}
+        Calendar cal = Calendar.getInstance();
+        String strValue = (String) value;
+        if (strValue.startsWith("{")) {
+            JSONObject jsonObject = JSONObject.toJsonObject(value);
+            long millis = jsonObject.getLong(TIME_IN_MILLIS);
+            // TODO: shouldn't TimezoneFlowTranslator mean this is not needed?
+            String timeZoneId = jsonObject.getString(TIMEZONE_ID);
+            cal.setTimeInMillis(millis);
+            cal.setTimeZone(TimeZone.getTimeZone(timeZoneId));
+        } else {
+            DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            // HACK (Bruno): using Date as an intermediate is a hack.
+            try {
+                Date d = dateFormat.parse(strValue);
+                cal.setTime(d);
+            } catch (Exception e) {
+                return null;
+            }
         }
-        return (K)cal;
-    }
+        return (K) cal;
+    }
+
     public Class<? extends Calendar> getClassToRender() {
         return Calendar.class;
-    }
+    }
+
     public IJsonWriter toJson(IJsonWriter jsonWriter, Calendar cal) {
         jsonWriter.object();
-        jsonWriter.keyValue(TIME_IN_MILLIS,cal.getTimeInMillis());
-        jsonWriter.keyValue(TIMEZONE_ID,cal.getTimeZone().getID());
+        jsonWriter.keyValue(TIME_IN_MILLIS, cal.getTimeInMillis());
+        jsonWriter.keyValue(TIMEZONE_ID, cal.getTimeZone().getID());
         return jsonWriter.endObject();
     }
 
